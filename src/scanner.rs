e7ee87e5ce4ee55a8551e68121c0bc5217c7dc9b
Alfredo Arvelaez <alfredoprograma.dev@gmail.com>
2024-01-09 12:12:56 -0400

    feature: implement methods for location state updating
    implement update line method
    imlement update offset method

--- src/scanner.rs
+++ src/scanner.rs
@@ -2,7 +2,7 @@
 
 use std::{error::Error, fmt};
 
-/// Contains information about error occurred during the scanning proccess
+/// Contains information about error occurred during the scanning proccess.
 #[derive(Debug)]
 pub struct ScannerError {
     message: &'static str,
@@ -21,15 +21,15 @@ impl fmt::Display for ScannerError {
     }
 }
 
-/// Keeps track of the position of the parsing proccess
+/// Keeps track of the position of the parsing proccess.
 pub struct Location {
-    /// current line
+    /// Current line.
     line: usize,
 
-    /// current offset for the current line
+    /// Current offset for the current line.
     line_offset: usize,
 
-    /// global offset with beggining
+    /// Global offset with beggining.
     cursor: usize,
 }
 
@@ -43,22 +43,37 @@ impl Location {
     }
 }
 
-/// Encapsulates scanning states and performs scanning operations over source code
+impl Location {
+    /// Increases line counter and resets line offset.
+    pub fn update_line(&mut self) {
+        self.line += 1;
+        self.line_offset = 0;
+    }
+
+    /// Increases global cursor and current line offset by a characters consumed amount
+    pub fn update_offset(&mut self, consumed: usize) {
+        self.cursor += consumed;
+        self.line_offset += consumed
+    }
+}
+
+/// Encapsulates scanning states and performs scanning operations over source code.
+///
 /// Basically, the `Scanner` is the core element of the source code transformation into
 /// meaningful structures for later interpreting.
 ///
-/// `Scanner` parses, tokenizes and register errors during the tokenization proccess
+/// `Scanner` parses, tokenizes and register errors during the tokenization proccess.
 pub struct Scanner<'a> {
-    /// source code to scan
+    /// Source code to scan.
     source: &'a str,
 
-    /// parsing location
+    /// Parsing location.
     location: Location,
 
-    /// generated tokens
+    /// Generated tokens.
     tokens: Vec<String>,
 
-    /// registered errors
+    /// Registered errors.
     errors: Vec<ScannerError>,
 }
 
@@ -73,7 +88,7 @@ impl<'a> Scanner<'a> {
     }
 }
 
-/// helper methods for handle internal scanner processes
+/// Helper methods for handle internal scanner processes.
 impl<'a> Scanner<'a> {
     fn is_end(&self) -> bool {
         self.location.cursor >= self.source.len()
@@ -101,6 +116,48 @@ mod tests {
             location.line_offset, 0,
             "should be initialized with line offset at position zero"
         )
+    }
+
+    #[test]
+    fn update_line_location() {
+        const RND_LINE: usize = 4;
+        const RND_LINE_OFFSET: usize = 22;
+
+        let mut location = Location::new();
+
+        // check concrete line and line offset state
+        location.line = RND_LINE;
+        location.line_offset = RND_LINE_OFFSET;
+
+        location.update_line();
+
+        assert_eq!(
+            location.line,
+            RND_LINE + 1,
+            "should update line counter by one"
+        );
+        assert_eq!(
+            location.line_offset, 0,
+            "should reset line offset after update line"
+        );
+    }
+
+    #[test]
+    fn update_offset_location() {
+        const RND_OFFSET: usize = 25;
+        let mut location = Location::new();
+
+        location.update_offset(RND_OFFSET);
+
+        assert_eq!(
+            location.line_offset, RND_OFFSET,
+            "should update line offset by given consumed characters amount"
+        );
+
+        assert_eq!(
+            location.cursor, RND_OFFSET,
+            "should global cursor by given consumed characters amount"
+        );
     }
 
     #[test]
